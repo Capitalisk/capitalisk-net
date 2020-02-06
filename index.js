979ab5d96482a9c2dc32d422e72c3036df45cd7e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-06 18:50:53 +0100

    Progress

---
+++ index.js
@@ -0,0 +1,343 @@
+const crypto = require('crypto');
+const {
+  P2P,
+  events: {
+    EVENT_NETWORK_READY,
+    EVENT_NEW_INBOUND_PEER,
+    EVENT_CLOSE_INBOUND,
+    EVENT_CLOSE_OUTBOUND,
+    EVENT_CONNECT_OUTBOUND,
+    EVENT_DISCOVERED_PEER,
+    EVENT_FAILED_TO_FETCH_PEER_INFO,
+    EVENT_FAILED_TO_PUSH_NODE_INFO,
+    EVENT_OUTBOUND_SOCKET_ERROR,
+    EVENT_INBOUND_SOCKET_ERROR,
+    EVENT_UPDATED_PEER_INFO,
+    EVENT_FAILED_PEER_INFO_UPDATE,
+    EVENT_REQUEST_RECEIVED,
+    EVENT_MESSAGE_RECEIVED,
+    EVENT_BAN_PEER,
+    EVENT_UNBAN_PEER
+  }
+} = require('@liskhq/lisk-p2p');
+const { lookupPeersIPs } = require('./utils');
+
+class LeaseholdNet {
+  constructor() {
+    this.channel = null;
+    this.options = {};
+    this.logger = null;
+    this.secret = null;
+  }
+
+  get alias() {
+    return 'leasehold_net';
+  }
+
+  get dependencies() {
+    return [];
+  }
+
+  get events() {
+    return ['event'];
+  }
+
+  get actions() {
+    return {
+      request: {
+        handler: async (action) => {
+          return this.p2p.request({
+            procedure: action.params.procedure,
+            data: action.params.data,
+          });
+        }
+      },
+      send: {
+        handler: async (action) => {
+          return this.p2p.send({
+            event: action.params.event,
+            data: action.params.data,
+          });
+        }
+      },
+      broadcast: {
+        handler: async (action) => {
+          return this.p2p.broadcast({
+            event: action.params.event,
+            data: action.params.data,
+          });
+        }
+      },
+      getConnectedPeers: {
+        handler: () => this.p2p.getConnectedPeers()
+      },
+      getDisconnectedPeers: {
+        handler: () => this.p2p.getDisconnectedPeers()
+      },
+      requestFromPeer: {
+        handler: async (action) => {
+          return this.p2p.requestFromPeer(
+            {
+              procedure: action.params.procedure,
+              data: action.params.data,
+            },
+            action.params.peerId,
+          );
+        }
+      },
+      sendToPeer: {
+        handler: async (action) => {
+          return this.p2p.sendToPeer(
+            {
+              event: action.params.event,
+              data: action.params.data,
+            },
+            action.params.peerId,
+          );
+        }
+      },
+      applyPenalty: {
+        handler: async (action) => {
+          return this.p2p.applyPenalty({
+            peerId: action.params.peerId,
+            penalty: action.params.penalty,
+          });
+        }
+      }
+    };
+  }
+
+  async load(channel, options, logger) {
+    this.channel = channel;
+    this.options = options;
+    this.logger = logger;
+
+    // TODO 2: Save and load previous peers from a JSON file.
+
+    this.secret = crypto.randomBytes(4).readUInt32BE(0);
+
+    let sanitizeNodeInfo = nodeInfo => ({
+      ...nodeInfo,
+      wsPort: this.options.wsPort,
+      advertiseAddress: this.options.advertiseAddress
+    });
+
+    let initialNodeInfo = sanitizeNodeInfo(
+      await this.channel.invoke('app:getApplicationState')
+    );
+
+    let seedPeers = await lookupPeersIPs(this.options.seedPeers, true);
+    let blacklistedIPs = this.options.blacklistedIPs || [];
+
+    let fixedPeers = this.options.fixedPeers
+      ? this.options.fixedPeers.map(peer => ({
+          ipAddress: peer.ip,
+          wsPort: peer.wsPort
+        }))
+      : [];
+
+    let whitelistedPeers = this.options.whitelistedPeers
+      ? this.options.whitelistedPeers.map(peer => ({
+          ipAddress: peer.ip,
+          wsPort: peer.wsPort
+        }))
+      : [];
+
+    this.p2p = new P2P({
+      nodeInfo: initialNodeInfo,
+      hostIp: this.options.hostIp,
+      blacklistedIPs,
+      fixedPeers,
+      whitelistedPeers,
+      seedPeers: seedPeers.map(peer => ({
+        ipAddress: peer.ip,
+        wsPort: peer.wsPort,
+      })),
+      maxOutboundConnections: this.options.maxOutboundConnections,
+      maxInboundConnections: this.options.maxInboundConnections,
+      peerBanTime: this.options.peerBanTime,
+      populatorInterval: this.options.populatorInterval,
+      sendPeerLimit: this.options.sendPeerLimit,
+      maxPeerDiscoveryResponseLength: this.options
+        .maxPeerDiscoveryResponseLength,
+      maxPeerInfoSize: this.options.maxPeerInfoSize,
+      wsMaxPayload: this.options.wsMaxPayload,
+      secret: this.secret
+    });
+
+    this.channel.subscribe('app:state:updated', event => {
+      let newNodeInfo = sanitizeNodeInfo(event.data);
+      try {
+        this.p2p.applyNodeInfo(newNodeInfo);
+      } catch (error) {
+        this.logger.error(
+          `Applying NodeInfo failed because of error: ${error.message ||
+            error}`
+        );
+      }
+    });
+
+    this.p2p.on(EVENT_NETWORK_READY, () => {
+      this.logger.debug('Node connected to the network');
+      this.channel.publish('network:ready');
+    });
+
+    this.p2p.on(EVENT_CLOSE_OUTBOUND, closePacket => {
+      this.logger.debug(
+        {
+          ipAddress: closePacket.peerInfo.ipAddress,
+          wsPort: closePacket.peerInfo.wsPort,
+          code: closePacket.code,
+          reason: closePacket.reason,
+        },
+        'EVENT_CLOSE_OUTBOUND: Close outbound peer connection'
+      );
+    });
+
+    this.p2p.on(EVENT_CLOSE_INBOUND, closePacket => {
+      this.logger.debug(
+        {
+          ipAddress: closePacket.peerInfo.ipAddress,
+          wsPort: closePacket.peerInfo.wsPort,
+          code: closePacket.code,
+          reason: closePacket.reason,
+        },
+        'EVENT_CLOSE_INBOUND: Close inbound peer connection'
+      );
+    });
+
+    this.p2p.on(EVENT_CONNECT_OUTBOUND, peerInfo => {
+      this.logger.debug(
+        {
+          ipAddress: peerInfo.ipAddress,
+          wsPort: peerInfo.wsPort,
+        },
+        'EVENT_CONNECT_OUTBOUND: Outbound peer connection'
+      );
+    });
+
+    this.p2p.on(EVENT_DISCOVERED_PEER, peerInfo => {
+      this.logger.trace(
+        {
+          ipAddress: peerInfo.ipAddress,
+          wsPort: peerInfo.wsPort,
+        },
+        'EVENT_DISCOVERED_PEER: Discovered peer connection'
+      );
+    });
+
+    this.p2p.on(EVENT_NEW_INBOUND_PEER, peerInfo => {
+      this.logger.debug(
+        {
+          ipAddress: peerInfo.ipAddress,
+          wsPort: peerInfo.wsPort,
+        },
+        'EVENT_NEW_INBOUND_PEER: Inbound peer connection'
+      );
+    });
+
+    this.p2p.on(EVENT_FAILED_TO_FETCH_PEER_INFO, error => {
+      this.logger.error(error.message || error);
+    });
+
+    this.p2p.on(EVENT_FAILED_TO_PUSH_NODE_INFO, error => {
+      this.logger.trace(error.message || error);
+    });
+
+    this.p2p.on(EVENT_OUTBOUND_SOCKET_ERROR, error => {
+      this.logger.debug(error.message || error);
+    });
+
+    this.p2p.on(EVENT_INBOUND_SOCKET_ERROR, error => {
+      this.logger.debug(error.message || error);
+    });
+
+    this.p2p.on(EVENT_UPDATED_PEER_INFO, peerInfo => {
+      this.logger.trace(
+        {
+          ipAddress: peerInfo.ipAddress,
+          wsPort: peerInfo.wsPort,
+        },
+        'EVENT_UPDATED_PEER_INFO: Update peer info',
+        JSON.stringify(peerInfo)
+      );
+    });
+
+    this.p2p.on(EVENT_FAILED_PEER_INFO_UPDATE, error => {
+      this.logger.error(error.message || error);
+    });
+
+    this.p2p.on(EVENT_REQUEST_RECEIVED, async request => {
+      this.logger.trace(
+        `EVENT_REQUEST_RECEIVED: Received inbound request for procedure ${request.procedure}`
+      );
+      // If the request has already been handled internally by the P2P library, we ignore.
+      if (request.wasResponseSent) {
+        return;
+      }
+      let hasTargetModule = hasNamespaceReg.test(request.procedure);
+      // If the request has no target module, default to chain (to support legacy protocol).
+      let sanitizedProcedure = hasTargetModule
+        ? request.procedure
+        : `chain:${request.procedure}`;
+      try {
+        let result = await this.channel.invokePublic(sanitizedProcedure, {
+          data: request.data,
+          peerId: request.peerId,
+        });
+        this.logger.trace(
+          `Peer request fulfilled event: Responded to peer request ${request.procedure}`
+        );
+        request.end(result); // Send the response back to the peer.
+      } catch (error) {
+        this.logger.error(
+          `Peer request not fulfilled event: Could not respond to peer request ${
+            request.procedure
+          } because of error: ${error.message || error}`
+        );
+        request.error(error); // Send an error back to the peer.
+      }
+    });
+
+    this.p2p.on(EVENT_MESSAGE_RECEIVED, async packet => {
+      this.logger.trace(
+        `EVENT_MESSAGE_RECEIVED: Received inbound message from ${packet.peerId} for event ${packet.event}`
+      );
+      this.channel.publish('network:event', packet);
+    });
+
+    this.p2p.on(EVENT_BAN_PEER, peerId => {
+      this.logger.error(
+        { peerId },
+        'EVENT_MESSAGE_RECEIVED: Peer has been banned temporarily'
+      );
+    });
+
+    this.p2p.on(EVENT_UNBAN_PEER, peerId => {
+      this.logger.error(
+        { peerId },
+        'EVENT_MESSAGE_RECEIVED: Peer ban has expired'
+      );
+    });
+
+    try {
+      await this.p2p.start();
+    } catch (error) {
+      this.logger.fatal(
+        {
+          message: error.message,
+          stack: error.stack,
+        },
+        `Failed to initialize net module`
+      );
+      process.emit('cleanup', error);
+    }
+  }
+
+  async unload() {
+    this.logger.info(`Stopping net module...`);
+    return this.p2p.stop();
+  }
+};
+
+module.exports = LeaseholdNet;
